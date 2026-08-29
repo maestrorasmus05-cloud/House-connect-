f6795bba17ac900f4bf5292145882d04d4c09755
maestrorasmus05-cloud <maestrorasmus05@gmail.com>
2026-08-29 15:12:08 +0200

    Add configuration settings for Supabase and Stripe

---
+++ config.js
@@ -0,0 +1,6 @@
+window.HC_CONFIG = {
+  SUPABASE_URL: 'https://srovaeszgajhrzedbptr.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_YEAIwC1Oys7RTX7t8HAedQ_BmYUB3Y7',
+  STRIPE_PRICE_ID: 'price_1U9PCu1D9nhyFu6jJOdxQCRx',
+  SITE_URL: 'https://maestrorasmus05-cloud.github.io/House-connect-'
+};

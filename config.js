3aa9bd53828787dd333b70c5064f2769d12e20dc
maestrorasmus05-cloud <maestrorasmus05@gmail.com>
2026-08-29 18:10:17 +0200

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.HC_CONFIG = {
-  SUPABASE_URL: 'https://gukzoovzpwmlpdsenkj.supabase.co',
+  SUPABASE_URL: 'https://gukzoovzpwmlpdseinkj.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1a3pvb3Z6cHdtbHBkc2VpbmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDQ2NTIsImV4cCI6MjEwMzQ4MDY1Mn0.mJ_HY4JMO9dRLJ9c_CAfHHw8wyWCP_zdWU86K3-AL4k',
   STRIPE_PRICE_ID: 'price_1U9PCu1D9nhyFu6jJOdxQCRx',
   SITE_URL: 'https://maestrorasmus05-cloud.github.io/House-connect-'
